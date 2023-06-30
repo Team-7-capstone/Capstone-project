8a51c0ab7892701b894a09a4dc2dc90b46b47943
Ivan <ivandeleon@me.com>
2023-06-30 16:56:14 -0400

    25% done

--- src/components/LikeButton/index.jsx
+++ src/components/LikeButton/index.jsx
@@ -1,13 +1,19 @@
 import React, { useMemo, useState } from "react";
 import { likePost, getLikesByUser } from "../../../api/FirestoreAPI";
 import "./index.scss";
-import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { AiOutlineHeart, AiFillHeart, AiOutlineComment } from "react-icons/ai";
 
 export default function LikeButton({ userId, postId }) {
   const [likesCount, setLikesCount] = useState(0);
+  const [showCommentBox, setShowCommentBox] = useState(false);
   const [liked, setLiked] = useState(false); // posts initially unliked
+  const [comment, setComment] = useState("");
   const handleLike = () => {
     likePost(userId, postId);
+  };
+
+  const getComment = (event) => {
+    setComment(event.target.value)
   };
 
   useMemo(() => {
@@ -15,20 +21,48 @@ export default function LikeButton({ userId, postId }) {
   }, [userId, postId]);
 
   return (
-    <div className="like-container" onClick={handleLike}>
+    <div className="like-container">
       <p>{likesCount} People Like this Post</p>
-      <div>
+      <div className="hr-line">
         <hr />
       </div>
-      <div className="likes-inner">
-        {liked ? (
-          <AiFillHeart size={30} color="#0a66c2" />
-        ) : (
-          <AiOutlineHeart size={30} />
-        )}
+      <div className="like-comment">
+        <div className="likes-comment-inner" onClick={handleLike}>
+          {liked ? (
+            <AiFillHeart size={30} color="#0a66c2" />
+          ) : (
+            <AiOutlineHeart size={30} />
+          )}
 
-        <p className={liked ? "blue" : "black"}>Like</p>
+          <p className={liked ? "blue" : "black"}>Like</p>
+        </div>
+        <div
+          className="likes-comment-inner"
+          onClick={() => setShowCommentBox(true)}
+        >
+          {
+            <AiOutlineComment
+              size={30}
+              color={showCommentBox ? "#0a66c2" : "#212121"}
+            />
+          }
+
+          <p className={showCommentBox ? "blue" : "black"}>Comment</p>
+        </div>
       </div>
+      {showCommentBox ? (
+        <>
+          <input
+            onClick={getComment}
+            placeholder="Add a Comment"
+            className="comment-input"
+            name="comment"
+          />
+          <button className="add-comment-btn">Add a Comment</button>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
