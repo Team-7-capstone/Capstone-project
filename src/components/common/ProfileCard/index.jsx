020fced59a3b6f745aa0e6469392bbd07343d4e7
MRAlexandre-Web <mitchalexand@gmail.com>
2023-07-01 00:39:36 -0400

    Profile Tab complete

--- src/components/common/ProfileCard/index.jsx
+++ src/components/common/ProfileCard/index.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import './index.scss'
+import PostsCard from "../PostsCard";
+import { getStatus } from "../../../api/FirestoreAPI";
 
 const ProfileCard = ({ currentUser, onEdit}) => {
+    const [allStatuses, setAllStatus] = useState([]);
+
+    useMemo(() => {
+        getStatus(setAllStatus)
+    }, []);
+
     return (
         <>
             <div className="profile-card">
-                <div className="edit-btn">
-                    <button onClick={onEdit}>Edit</button>
+
+                <div className="profile-info">
+                    <div>
+                        <h3 className="username">{currentUser.name}</h3>
+                        <p className="userEmail">{currentUser.email}</p>
+                        <p className="heading">{currentUser.headline}</p>
+                        <p className="location">{currentUser.location}</p>
+                        <div className="edit-btn">
+                            <button onClick={onEdit}>Edit</button>
+                        </div>
+                    </div>
+                    <div className="right-info">
+                        <p className="expertise">Expertise: {currentUser.expertise}</p>
+                        <p className="intrest">Intrests: {currentUser.intrest}</p>
+                    </div>
                 </div>
-                <h3 className="username">{currentUser.name}</h3>
-                <p className="userEmail">{currentUser.email}</p>
             </div>
+
+            <div className="post-status-main">
+                {allStatuses.filter((item) => {
+                    return item.userEmail === localStorage.getItem("userEmail")
+                }).map((posts) => {
+                    return(
+                        <div key={posts.id}>
+                            <PostsCard posts={posts}/>
+                        </div>
+                    );
+                })}
+             </div>
 
         </>
     )
