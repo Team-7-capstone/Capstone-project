54f13a13dcb86749311b0ded39a6b0877857509c
Ivan <ivandeleon@me.com>
2023-07-06 16:52:31 -0400

    finished User Search Functionality

--- src/components/common/SearchUsers/index.jsx
+++ src/components/common/SearchUsers/index.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 import "./index.scss";
 
-export default function SearchUsers({ setIsSearch }) {
+export default function SearchUsers({ setIsSearch, setSearchInput }) {
   return (
     <div className="search-users">
-      <input placeholder="Search Users.." />
+      <input
+        placeholder="Search Users.."
+        onChange={(event) => setSearchInput(event.target.value)}
+      />
+      <AiOutlineCloseCircle
+        className="close-icon"
+        size={20}
+        onClick={() => {
+          setIsSearch(false);
+          setSearchInput("");
+        }}
+      />
     </div>
   );
 }
--- src/components/common/Topbar/index.jsx
+++ src/components/common/Topbar/index.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import LinkedinLogo from "../../../assets/linkedinLogo.png";
 import user from "../../../assets/user.png";
-import ProfilePopup from "../ProfilePopup";
 import SearchUsers from "../SearchUsers";
-
 import {
   AiOutlineSearch,
   AiOutlineHome,
@@ -13,11 +11,16 @@ import {
 } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import { BsBriefcase } from "react-icons/bs";
+import { getAllUsers } from "../../../api/FirestoreAPI";
+import ProfilePopup from "../ProfilePopup";
 import "./index.scss";
 
 export default function Topbar() {
   const [popupVisible, setPopupVisible] = useState(false);
   const [isSearch, setIsSearch] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [searchInput, setSearchInput] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState("");
 
   let navigate = useNavigate();
   const goToRoute = (route) => {
@@ -27,6 +30,40 @@ export default function Topbar() {
   const displayPop = () => {
     setPopupVisible(!popupVisible);
   };
+
+  const openUser = (user) => {
+    navigate("/profile", {
+      state: {
+        id: user.id,
+        email: user.email,
+      },
+    });
+  };
+
+  const handleSearch = () => {
+    if (searchInput !== "") {
+      let searched = users.filter((user) => {
+        return Object.values(user)
+          .join("")
+          .toLowerCase()
+          .includes(searchInput.toLowerCase());
+      });
+      setFilteredUsers(searched);
+    } else {
+      setFilteredUsers(users);
+    }
+  };
+
+  useEffect(() => {
+    let debounced = setTimeout(() => {
+      handleSearch();
+    }, 1000);
+    return () => clearTimeout(debounced);
+  }, [searchInput]);
+
+  useEffect(() => {
+    getAllUsers(setUsers);
+  }, []);
 
   return (
     <div className="topbar-main">
@@ -40,7 +77,10 @@ export default function Topbar() {
 
       <img className="linkedin-logo" src={LinkedinLogo} alt="LinkedinLogo" />
       {isSearch ? (
-        <SearchUsers />
+        <SearchUsers
+          setIsSearch={setIsSearch}
+          setSearchInput={setSearchInput}
+        />
       ) : (
         <div className="react-icons">
           <AiOutlineSearch
@@ -65,6 +105,23 @@ export default function Topbar() {
       )}
 
       <img className="user-logo" src={user} alt="user" onClick={displayPop} />
+
+      {searchInput.length === 0 ? (
+        <></>
+      ) : (
+        <div className="search-results">
+          {filteredUsers.length === 0 ? (
+            <div>No Results Found.. </div>
+          ) : (
+            filteredUsers.map((user) => (
+              <div className="search-inner" onClick={() => openUser(user)}>
+                <img src={user.imageLink} />
+                <p className="name">{user.name}</p>
+              </div>
+            ))
+          )}
+        </div>
+      )}
     </div>
   );
 }
