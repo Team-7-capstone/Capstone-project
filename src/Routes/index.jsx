41aaf50ed82a5e1c4e846063105e4143d9671be3
em2233 <emilypaye@gmail.com>
2023-07-06 15:28:33 -0400

    Revert "Building the Connections Page"
    This reverts commit 9ccbdcd17d12813993c272fb14d65b0c9d32173f.

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -4,7 +4,6 @@ import Login from "../Pages/Login";
 import Register from "../Pages/Register";
 import HomeLayout from "../layouts/HomeLayout";
 import ProfileLayout from "../layouts/ProfileLayout";
-import ConnectionLayout from "../layouts/ConnectionLayout";
 
 export const router = createBrowserRouter([
   {
@@ -22,9 +21,5 @@ export const router = createBrowserRouter([
   {
     path: "/profile",
     element: <ProfileLayout />,
-  },
-  {
-    path: "/connections",
-    element: <ConnectionLayout />,
   },
 ]);
