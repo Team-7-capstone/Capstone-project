4d84fffebdeba29c026ee31913126248bc52b53a
MRAlexandre-Web <mitchalexand@gmail.com>
2023-07-11 21:00:38 -0400

    Login and Register responsive design

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-
-import "../Sass/HomeComponent.scss";
 import PostStatus from "./common/PostUpdate";
 
 export default function HomeComponent({ currentUser }) {
   return (
     <div className="home-component">
-      <PostStatus currentUser = {currentUser}/>
+      <PostStatus currentUser={currentUser} />
     </div>
   );
 }
