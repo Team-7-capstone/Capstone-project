5b6c3383fc04657303deed6ad6806e4e94e59c91
Ivan Leon <127246357+LeontheSpecialist@users.noreply.github.com>
2023-07-17 12:46:15 -0400

    working audioPlayer again for the team

---
+++ public/data/tracks.js
@@ -0,0 +1,85 @@
+// audio files
+import elements from './LindseyStirling-Elements.mp3'
+import livin from './BonJovi-LivinOnAPrayer.mp3'
+import superstition from './StevieWonder-Superstition.mp3'
+import headinsouth from './ZachBryan_HeadinSouth.mp3'
+import beautiful from './a_beautiful_day.mp3';
+import world from './We_Are_The_World.mp3';
+import top from './dbang-world.mp3';
+import cinematic from './cinematic-time-lapse-115672.mp3';
+import forest from './forest-lullaby-110624.mp3';
+import podcast from './the-podcast-intro-111863.mp3';
+
+
+
+// audio thumbnails
+import lindsey from './LindseyStirlingAlbumCover.png'
+import bonjovi from './BonJovi-SlipperyWhenWet.png'
+import stevie from './StevieWonderAlbum-Superstition.png'
+import zach from './ZachBryan_Album_HeadinSouth.png'
+import lexin from './lexin.jpeg';
+import dbanj from './dbanj.png';
+import jackson from './jackson.jpeg';
+import trinix from './trinix.jpeg';
+
+
+export const tracks = [
+  {
+    title: 'Elements',
+    src: elements,
+    author: 'Lindsey Stirling',
+    thumbnail: lindsey,
+  },
+  {
+    title: 'Livin On A Prayer',
+    src: livin,
+    author: 'Jon BonJovi',
+    thumbnail: bonjovi,
+  },
+  {
+    title: 'Superstition',
+    src: superstition,
+    author: 'Stevie Wonder',
+    thumbnail: stevie,
+  },
+  {
+    title: 'Headin South',
+    src: headinsouth,
+    author: 'Zach Bryan',
+    thumbnail: zach,
+  },
+  {
+    title: 'Trinix ft Rushawn – Its a beautiful day',
+    src: beautiful,
+    author: 'Trinix ft Rushawn',
+    thumbnail: trinix,
+  },
+  {
+    title: 'Michael Jackson – We Are The World',
+    src: world,
+    author: 'Michael Jackson',
+    thumbnail: jackson,
+  },
+  {
+    title: 'D’banj -Top Of The World',
+    src: top,
+    author: 'Dbanj',
+    thumbnail: dbanj,
+  },
+  {
+    title: 'Cinematic Time Lapse',
+    src: cinematic,
+    author: 'Lexin Music',
+    thumbnail: lexin,
+  },
+  {
+    title: 'Forest Lullaby',
+    src: forest,
+    author: 'Lesfm',
+  },
+  {
+    title: 'The Podcast Intro',
+    src: podcast,
+    author: 'Music Unlimited',
+  },
+];
